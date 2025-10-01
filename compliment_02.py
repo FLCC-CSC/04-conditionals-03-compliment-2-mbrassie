02b5cb7aae12226993cddf52b17409fc7deb5a20
mbrassie <mbrassie2@fingerlakes.edu>
2025-10-01 10:17:15 -0400

    Update compliment_02.py

--- compliment_02.py
+++ compliment_02.py
@@ -16,7 +16,14 @@
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
 
-
+user = input("Would you like a compliment? ")
+yes = ("You have wonderful eyes.")
+if (user == 'yes'):
+    print(f"{yes}")
+    print("Thank you for playing.")
+else:
+    print("No compliment for you!")
+    print ("Thank you for playing.")
 
 
 
